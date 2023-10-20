4a97e105a41dca49213797d8728791b84a0c9265
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-20 12:38:15 +0900

    Update add_numbers.cbl

--- add_numbers.cbl
+++ add_numbers.cbl
@@ -1,14 +1,27 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. AddTwoNumbers.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. AddTwoNumbers.
 
-       DATA DIVISION.
-       WORKING-STORAGE SECTION.
-       01 Num1           PIC 9(5) VALUE 1.
-       01 Num2           PIC 9(5) VALUE 1.
-       01 Result         PIC 9(5).
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 Num1           PIC 9(5) VALUE 1.
+01 Num2           PIC 9(5) VALUE 1.
+01 Result         PIC 9(5).
 
-       PROCEDURE DIVISION.
-           COMPUTE Result = Num1 + Num2.
-           DISPLAY "1 + 1 = " Result.
+PROCEDURE DIVISION.
+COMPUTE Result = Num1 + Num2.
+DISPLAY "1 + 1 = " Result.
 
-           STOP RUN.
+STOP RUN.
+
+##########################
+
+IDENTIFICATION DIVISION.
+PROGRAM-ID. HelloWorld.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+
+PROCEDURE DIVISION.
+DISPLAY "Hello World!"
+
+STOP RUN.
