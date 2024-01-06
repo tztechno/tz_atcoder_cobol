24178b4b9b089ed9713d5dbfe99317a804780d67
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-06 15:42:17 +0900

    Update late.cbl

--- late.cbl
+++ late.cbl
@@ -2,30 +2,27 @@
 
 #####################################
 
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. ATCODER.
-       ENVIRONMENT DIVISION.
-       INPUT-OUTPUT SECTION.
-       FILE-CONTROL.
-           SELECT SYSIN ASSIGN TO KEYBOARD ORGANIZATION LINE SEQUENTIAL.
-       DATA DIVISION.
-       FILE SECTION.
-           FD SYSIN.
-               01 INDATA PIC X(2000000).
-       WORKING-STORAGE SECTION.
-           01 WK.
-               03 INP PIC X(100000).
-               03 D PIC 9(18).
-               03 S PIC 9(18).
-               03 T PIC 9(18).
-       PROCEDURE DIVISION.
-           OPEN INPUT SYSIN.
-           READ SYSIN INTO INP.
-           CLOSE SYSIN.
-           UNSTRING INP DELIMITED BY SPACE INTO D T S.
-           IF T * S >= D THEN DISPLAY "Yes" ELSE DISPLAY "No".
-           STOP RUN.
-       END PROGRAM ATCODER.
+IDENTIFICATION DIVISION.
+PROGRAM-ID. XXX.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 D          PIC 9(10).
+01 S          PIC 9(10).
+01 T          PIC 9(10).
+01 M          PIC 9(10).
+01 ln         PIC X(30).
+
+PROCEDURE DIVISION.
+  ACCEPT ln.
+  UNSTRING ln DELIMITED BY SPACE INTO D T S.
+  MULTIPLY T BY S GIVING M.
+  IF D <= M THEN
+    DISPLAY "Yes"
+  ELSE
+    DISPLAY "No"
+  END-IF.
+  STOP RUN.
 
 #####################################
 
