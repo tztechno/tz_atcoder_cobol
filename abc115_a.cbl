482147c7f3301bc2f35306ca979e7be210cc1418
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-10 16:51:00 +0900

    Create abc115_a.cbl

---
+++ abc115_a.cbl
@@ -0,0 +1,35 @@
+abc115_a.cbl
+##########################################
+##########################################
+##########################################
+##########################################
+##########################################
+##########################################
+##########################################
+##########################################
+IDENTIFICATION DIVISION.
+PROGRAM-ID. MAIN.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+  01 INPT       PIC X(30).
+  01 D          PIC 9(10).
+
+PROCEDURE DIVISION.
+  ACCEPT INPT.
+  UNSTRING INPT DELIMITED BY SPACE INTO D
+  IF D = 22
+    DISPLAY 'Christmas Eve Eve Eve'
+  ELSE IF D = 23
+    DISPLAY 'Christmas Eve Eve'
+  ELSE IF D = 24
+    DISPLAY 'Christmas Eve'
+  ELSE
+    DISPLAY 'Christmas'
+  END-IF.
+  STOP RUN.
+##########################################
+[python]
+D=int(input())
+print("Christmas"+" Eve"*(25-D))
+##########################################
