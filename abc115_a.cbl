7481e3bd9064a02da8141f3178784e8389c9caec
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-10 16:52:13 +0900

    Update abc115_a.cbl

--- abc115_a.cbl
+++ abc115_a.cbl
@@ -6,6 +6,27 @@ abc115_a.cbl
 ##########################################
 ##########################################
 ##########################################
+IDENTIFICATION DIVISION.
+PROGRAM-ID. PROGRAM_ID.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 D          PIC 9(10).
+
+PROCEDURE DIVISION.
+  ACCEPT D.
+  EVALUATE D
+    WHEN 25
+      DISPLAY "Christmas"
+    WHEN 24
+      DISPLAY "Christmas Eve"
+    WHEN 23
+      DISPLAY "Christmas Eve Eve"
+    WHEN OTHER
+      DISPLAY "Christmas Eve Eve Eve"
+  END-EVALUATE.
+  STOP RUN.
+
 ##########################################
 IDENTIFICATION DIVISION.
 PROGRAM-ID. MAIN.
