9fe297fbb12b19aa7f7cda0bed25b4d5726c4f1d
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-14 22:56:10 +0900

    Update abc115_a.cbl

--- abc115_a.cbl
+++ abc115_a.cbl
@@ -24,7 +24,6 @@ PROCEDURE DIVISION.
 ##########################################
 ##########################################
 ##########################################
-##########################################
 IDENTIFICATION DIVISION.
 PROGRAM-ID. MAIN.
 
@@ -80,8 +79,51 @@ PROCEDURE DIVISION.
   STOP RUN.
 ##########################################
 ##########################################
-[my ac]
+IDENTIFICATION DIVISION.
+PROGRAM-ID. MAIN.
 
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+  01 INPT       PIC X(30).
+  01 D          PIC 9(10).
+  01 E          PIC 9(10).
+  01 STR1    PIC X(9) VALUE "Christmas".
+  01 STR2    PIC X(3) VALUE "Eve".
+  01 RESULT  PIC X(100).
+  01 PT PIC 99 VALUE 1.
+  01 CT PIC 99 VALUE 1.
+
+PROCEDURE DIVISION.
+  ACCEPT INPT.
+  UNSTRING INPT DELIMITED BY SPACE INTO D.
+  COMPUTE E = 25 - D.
+
+  STRING 
+    STR1 DELIMITED BY SIZE
+    " " DELIMITED BY SIZE
+    INTO RESULT
+    WITH POINTER PT.
+
+  PERFORM VARYING CT FROM 1 BY 1 UNTIL CT > E
+    STRING 
+      STR2 DELIMITED BY SIZE
+      " "    DELIMITED BY SIZE
+      INTO RESULT
+      WITH POINTER PT
+  END-PERFORM.
+
+  DISPLAY RESULT.
+------------------------------------------
+D=int(input())
+E=25-D
+STR1='Christmas'
+STR2='Eve'
+RESULT=STR1
+for i in range(E):
+  RESULT+=' '+STR2
+print(RESULT)
+
+##########################################
 IDENTIFICATION DIVISION.
 PROGRAM-ID. MAIN.
 
@@ -100,9 +142,13 @@ PROCEDURE DIVISION.
   COMPUTE X = 9 + 4 * E
   DISPLAY ANS(1:X)
   STOP RUN.
-
+------------------------------------------
+D=int(input())
+E=25-D
+X=9+4*E
+ANS='Christmas Eve Eve Eve'
+print(ANS[0:X])
 ##########################################
-[my ac]
 
 IDENTIFICATION DIVISION.
 PROGRAM-ID. MAIN.
@@ -125,6 +171,16 @@ PROCEDURE DIVISION.
     DISPLAY 'Christmas'
   END-IF.
   STOP RUN.
+------------------------------------------
+D=int(input())
+if D==22:
+    print("Christmas Eve Eve Eve")
+elif D==23:
+    print("Christmas Eve Eve")
+elif D==24:
+    print("Christmas Eve")
+else:
+    print("Christmas")
 
 ##########################################
 [python]
