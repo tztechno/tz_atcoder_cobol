083be79a8be83c239d6d8a0830e3fb8fc059da83
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-07-14 22:25:06 +0900

    Update abc115_a.cbl

--- abc115_a.cbl
+++ abc115_a.cbl
@@ -1,10 +1,62 @@
 abc115_a.cbl
 ##########################################
+PROGRAM-ID. MAIN.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+01 WS-STR1 PIC X(5) VALUE "Hello".
+01 WS-STR2 PIC X(5) VALUE "World".
+01 WS-RESULT PIC X(30).
+01 WS-POINTER PIC 99 VALUE 1.
+
+PROCEDURE DIVISION.
+    STRING WS-STR1 DELIMITED BY SIZE
+           " "    DELIMITED BY SIZE
+           WS-STR2 DELIMITED BY SIZE
+        INTO WS-RESULT
+        WITH POINTER WS-POINTER
+        ON OVERFLOW
+            DISPLAY "String overflow occurred"
+    END-STRING.
+
+    DISPLAY WS-RESULT.
 ##########################################
 ##########################################
 ##########################################
 ##########################################
 ##########################################
+IDENTIFICATION DIVISION.
+PROGRAM-ID. MAIN.
+
+DATA DIVISION.
+WORKING-STORAGE SECTION.
+  01 INPT       PIC X(30).
+  01 D          PIC 9(10).
+  01 E          PIC 9(10).
+  01 WS-STR1    PIC X(9) VALUE "Christmas".
+  01 WS-STR2    PIC X(3) VALUE "Eve".
+  01 WS-RESULT  PIC X(100).
+  01 WS-POINTER PIC 99 VALUE 1.
+  01 WS-COUNTER PIC 99 VALUE 1.
+
+PROCEDURE DIVISION.
+  ACCEPT INPT.
+  UNSTRING INPT DELIMITED BY SPACE INTO D.
+  COMPUTE E = 25 - D.
+
+  STRING WS-STR1 DELIMITED BY SIZE
+         " "    DELIMITED BY SIZE
+    INTO WS-RESULT
+    WITH POINTER WS-POINTER.
+
+  PERFORM VARYING WS-COUNTER FROM 1 BY 1 UNTIL WS-COUNTER > E
+    STRING WS-STR2 DELIMITED BY SIZE
+           " "    DELIMITED BY SIZE
+      INTO WS-RESULT
+      WITH POINTER WS-POINTER
+  END-PERFORM.
+
+  DISPLAY WS-RESULT.
 ##########################################
 IDENTIFICATION DIVISION.
 PROGRAM-ID. PROGRAM_ID.
