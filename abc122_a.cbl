f83c993a865d4ddaebf79733a17e78fa9bf5fddb
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-08-28 16:02:14 +0900

    Update abc122_a.cbl

--- abc122_a.cbl
+++ abc122_a.cbl
@@ -6,32 +6,6 @@ abc122_a.cbl
 ##########################################
 ##########################################
 ##########################################
-IDENTIFICATION DIVISION.
-PROGRAM-ID. MAIN.
-ENVIRONMENT DIVISION.
-DATA DIVISION.
-WORKING-STORAGE SECTION.
-   01 INP              PIC X(1).
-   01 ANS              PIC X(1).
-   01 TABLE-SIZE       PIC 9 VALUE 4.  *> Table size of 4
-   01 MAP-TABLE.
-      05 MAP-KEYS      PIC X OCCURS 4 TIMES VALUE 'ACTG'. *> The keys: A, C, T, G
-      05 MAP-VALUES    PIC X OCCURS 4 TIMES VALUE 'TGAC'. *> The corresponding values: T, G, A, C
-   01 INDEX-COUNTER    PIC 9.
-
-PROCEDURE DIVISION.
-   ACCEPT INP.
-
-   *> Find the matching key
-   PERFORM VARYING INDEX-COUNTER FROM 1 BY 1 UNTIL INDEX-COUNTER > TABLE-SIZE
-       IF INP = MAP-KEYS (INDEX-COUNTER)
-           MOVE MAP-VALUES (INDEX-COUNTER) TO ANS
-           EXIT PERFORM
-       END-IF
-   END-PERFORM
-
-   DISPLAY ANS.
-   STOP RUN.
 ##########################################
 IDENTIFICATION DIVISION.
 PROGRAM-ID. MAIN.
